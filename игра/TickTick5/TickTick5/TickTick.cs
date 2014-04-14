7bbb3be79357107ad8eb991c36352a3bad4c31a3
maksibonus <maksibonus@mail.ru>
2014-04-14 17:32:17 +0300

    somethingChange

--- игра/TickTick5/TickTick5/TickTick.cs
+++ игра/TickTick5/TickTick5/TickTick.cs
@@ -28,8 +28,9 @@ class TickTick : GameEnvironment
         gameStateManager.AddGameState("levelMenu", new LevelMenuState());
         gameStateManager.AddGameState("gameOverState", new GameOverState());
         gameStateManager.AddGameState("levelFinishedState", new LevelFinishedState());
+        gameStateManager.AddGameState("questionState", new QuestionState());
         gameStateManager.SwitchTo("titleMenu");
 
-        AssetManager.PlayMusic("Sounds/snd_music");
+        //AssetManager.PlayMusic("Sounds/snd_music");
     }
 }
