15c6f1de49c80b09285e4c63cad65747b44e24e1
maksibonus <maksibonus@mail.ru>
2014-04-14 21:26:14 +0300

    авав

--- игра/TickTick5/TickTick5/TickTick.cs
+++ игра/TickTick5/TickTick5/TickTick.cs
@@ -7,14 +7,6 @@ class TickTick : GameEnvironment
     public static TickTick game = new TickTick();
     static void Main()
     {
-<<<<<<< HEAD
-=======
-//<<<<<<< HEAD
->>>>>>> 22c863c66b99ab5c7a18498048e9070d4ea9bf2c
-        //GameTests.TestManager.Initialize();
-//=======
-        GameTests.TestManager.Initialize();
-//>>>>>>> 64fbed2b7a4a5ce3aefc58bbe199204b6b34fca4
         TickTick game = new TickTick();
         game.Run();
     }
